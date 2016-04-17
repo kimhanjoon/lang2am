30cf2c8868290a19f234f9af3e45a487fd577295
Kim, Hanjoon <kimhanjoon@gmail.com>
2016-04-18 02:02:52 +0900

    text html entity escape

--- lang2am-web/src/main/webapp/main.js
+++ lang2am-web/src/main/webapp/main.js
@@ -13,7 +13,7 @@ function copyToClipboard(text) {
     $temp.remove();
 }
 function highlight(text, queryRegExp) {
-	return text.replace(queryRegExp, '<span class="yellow">$1</span>')
+	return text.replace(queryRegExp, '<span class="yellow">$1</span>');
 }
 Handlebars.registerHelper('nvl', function(first, second) {
 	return first ? first : second;
@@ -38,7 +38,7 @@ $(function() {
 		$("#btnCancelNewcode").click();
 		$("#search").focus();
 	});
-	
+
 	//XXX pre-complie
 	var translate_table;
 	$.get("text_table.hbs", function(template_text){
@@ -55,8 +55,8 @@ $(function() {
 	$("#btnSaveNewcode").click(function() {
 		$.post("text/", {
 			code: $("#code").val(),
-			ko: $("#ko").val(), 
-			en: $("#en").val(), 
+			ko: $("#ko").val(),
+			en: $("#en").val(),
 			zh: $("#zh").val(),
 		})
 		.done(function(data) {
@@ -71,14 +71,14 @@ $(function() {
     		Materialize.toast('Failed : ' + jqXHR.responseJSON.message, 3000, "red accent-3");
 		});
 	});
-	
+
     $('#newcode_table').pasteImageReader(function(result) {
     	var $img = $("<img></img>");
     	$img.attr("src", result.dataURL);
     	$img.attr("height", "70px");
     	$("#saveImage").append($img);
     });
-	
+
 	var _last_query = "";
 	var _last_queryRegExp = new RegExp("("+escapeRegExp(_last_query)+")", "gi");;
 	search_texts("");
@@ -88,12 +88,12 @@ $(function() {
 		if( _last_query !== query ) {
 			search_texts(query);
 		}
-		
+
 	}, 1000));
-	
+
 	function search_texts(query) {
 		_last_query = query;
-		var queryRegExp = new RegExp("("+escapeRegExp(query)+")", "gi");
+		var queryRegExp = new RegExp("("+escapeRegExp(_.escape(query))+")", "gi");
 		console.log(query, " => ", queryRegExp);
 		_last_queryRegExp = queryRegExp;
 		$("#texts_table").empty();
@@ -104,7 +104,13 @@ $(function() {
     		dataType: "json",
     	})
     	.done(function(data) {
-    		
+
+			_.each(data.textlist, function(e) {
+				e.textEn = _.escape(e.textEn);
+				e.textKo = _.escape(e.textKo);
+				e.textZh = _.escape(e.textZh);
+			});
+
     		if( query && query.length > 0 ) {
     			_.each(data.textlist, function(e) {
     				e.code_highlight = highlight(e.code, queryRegExp);
@@ -132,7 +138,7 @@ $(function() {
 	$("i.refresh").click(function(e) {
 		search_texts(_last_query);
 	});
-	
+
 	$("#texts_table").on("click", ".source-code a", function(e) {
 		copyToClipboard($(this).find("span").text());
 	});
@@ -146,7 +152,7 @@ $(function() {
 	$.get("edittext.hbs", function(template_text){
 		edittext_table = Handlebars.compile(template_text);
 	});
-	
+
 	$("#texts_table").on("click", "i.edit", function(e) {
 		var $td = $(this).closest("td");
 		var code = $td.closest("tr").data("code");
@@ -157,14 +163,14 @@ $(function() {
 			$td.find("textarea").focus();
 		});
 	});
-	
+
 	$("#texts_table").on("click", ".cancel-edittext", function(e) {
 		var $td = $(this).closest("td");
 		$td.find(".text-edittext, .edittext").remove();
 		$td.find(".text").show();
 		$td.find("i.edit").css('display','');
 	});
-	
+
 	$("#texts_table").on("click", ".save-edittext", function(e) {
 		var $td = $(this).closest("td");
 		var code = $td.closest("tr").data("code");
